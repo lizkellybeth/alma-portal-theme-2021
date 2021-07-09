c2a225f70e45de5422c96b6059d4f0c911275d2b
Elizabeth Sharp <esharp@nrao.edu>
2021-07-08 21:52:18 -0400

    added new theme files.

---
+++ alma/js/alma-arc-localization.js
@@ -0,0 +1,100 @@
+$( document ).ready(function() {
+    doLocalization();
+    resizeAlmaPortlets();
+});
+    
+function doLocalization(){
+    var url = window.location.href; 
+    console.log("doLocalization for: " + url);
+    var esoPortlet = $("a:contains('EU ARC News')").parent().parent().parent();  
+    var naojPortlet = $("a:contains('NAOJ News')").parent().parent().parent();  
+    var nraoPortlet = $("a:contains('NRAO News')").parent().parent().parent(); 
+    //alert($("a:contains('NAOJ News')").attr("href"));
+    renameNewsPortlet();
+
+        
+    if (url.indexOf("eso.org") >= 0){
+        console.log("found ESO!");
+        naojPortlet.remove();
+        nraoPortlet.remove();
+        $('#arclogo-img').attr('src','++theme++alma-prototype-theme/images/logo-eu.png');
+        $('#almalogo-anchor').attr('href','/');
+        return;
+    }
+    if (url.indexOf("nrao.edu") >= 0){
+        console.log("found NRAO!");
+        naojPortlet.remove();
+        esoPortlet.remove();
+        $('#arclogo-img').attr('src','++theme++alma-prototype-theme/images/logo-na-pride.png');
+        $('#almalogo-anchor').attr('href','/');
+        return;
+    }
+    if (url.indexOf("nao.ac.jp") >= 0){
+        console.log("found NAOJ!");
+        esoPortlet.remove();
+        nraoPortlet.remove();
+        $('#arclogo-img').attr('src','++theme++alma-prototype-theme/images/NAOJ_logo.png');
+        $('#almalogo-anchor').attr('href','/');
+        return;
+    }
+    console.log("not sure what to localize! Defaulting to JAO...");
+    //$('#arclogo-img').attr('src','++theme++alma-prototype-theme/images/logo-jao.png');
+    //$('#almalogo-anchor').attr('href','/portal');
+    $(document).prop('title', 'ALMA DEV Portal');
+    naojPortlet.remove();
+    esoPortlet.remove();
+    return;    
+}
+
+function renameNewsPortlet(){
+    console.log("rename news portlet...");
+    $('.portletNews > dt:nth-child(1) > a:nth-child(2)').text("Observatory News") ;   
+}
+
+// deprecated
+function setArcLogo(){
+    var url = window.location.href;
+    if (url.indexOf("almascience.eso.org") > -1){
+        $('#arclogo-img').attr('src','++theme++alma-prototype-theme/images/logo-eu.png');
+    } else if (url.indexOf("almascience.nrao.edu") > -1) {
+        $('#arclogo-img').attr('src','++theme++alma-prototype-theme/images/logo-na.png');
+    } else if (url.indexOf("almascience.nao.ac.jp") > -1) {
+        $('#arclogo-img').attr('src','++theme++alma-prototype-theme/images/logo-ea.png');
+    } else {
+        $('#arclogo-img').attr('src','++theme++alma-prototype-theme/images/logo-jao.png');
+    }
+}
+
+function resizeAlmaPortlets(){
+    console.log("resizeAlmaPortlets()");
+
+    var widthOfDiv = $("#alma-news-div").width();
+    console.log("widthOfDiv: " + widthOfDiv);
+    
+    var mgmtPortletsLinkWidth = $("div.managePortletsLink:nth-child(4) > a:nth-child(1)").width();
+    console.log("mgmtPortletsLinkWidth: " + mgmtPortletsLinkWidth);
+    
+    var availableWidth = widthOfDiv;
+    if (mgmtPortletsLinkWidth == null){  
+        console.log("mgmtPortletsLink IS null!");
+    } else {
+        console.log("mgmtPortletsLink not null!");
+        availableWidth = availableWidth - mgmtPortletsLinkWidth;
+    }
+    console.log("availableWidth minus space used by the mgmtPortletsLinkWidth: " + availableWidth);
+    
+    var numPortlets = $("#alma-news-div .portlet").length;
+    console.log("numPortlets in alma-news div: " + numPortlets);
+    
+    var portletWidth = 30;//estimated
+    console.log("portletWidth: " + portletWidth);
+    
+    availableWidth = availableWidth - (portletWidth * numPortlets);
+    console.log("availableWidth minus space used by the portlets: " + availableWidth);
+    
+    var adjustedPortletWidth = availableWidth / numPortlets;
+    console.log("adjustedPortletWidth: " + adjustedPortletWidth);
+        
+    $("#alma-news-div .portletWrapper.home").width(adjustedPortletWidth);
+  
+}
---
+++ alma/js/alma.js
@@ -0,0 +1,80 @@
+var ALMA_BASE_URL = "/";
+$( document ).ready(function() {
+    if ((window.location.pathname == "/portal") || (window.location.pathname == "/portal/")){
+        ALMA_BASE_URL = "/portal/";
+        displayHomePage();
+        
+    } else if (window.location.pathname === "/"){
+        displayHomePage();
+        
+    } else {
+        displayDefault();
+    }
+});
+
+function displayHomePage(){
+    //alert("displayHomePage()");
+    getScienceHighlightURL();
+    $("#main-container").addClass("home");
+    $("#alma-news-div").addClass("home");
+    $("#alma-sci-hi").addClass("home");
+    $("#alma-col1").addClass("home");
+    $("#alma-central").addClass("home");
+    $(".portletWrapper").addClass("home");
+}
+
+function displayDefault(){
+    //alert("displayDefault()");
+    $("#main-container").removeClass("home");
+    $("#alma-news-div").removeClass("home");
+    $("#alma-sci-hi").removeClass("home");
+    $("alma-col1").removeClass("home");
+    $("#alma-central").removeClass("home");
+    $(".portletWrapper").removeClass("home");
+}
+
+function getScienceHighlight(shURL){
+    console.log( "getScienceHighlight()");
+    console.log( "shURL: " + shURL);
+    $.ajax({
+        url: shURL,
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('Accept', 'application/json');
+        },
+        success: function(data) {
+            shout = data;
+            console.log(data["text"]["data"]);
+            link = data["@id"]
+            content = data["text"]["data"];
+            image = data["image"]["download"];
+            image_caption = data["image_caption"];
+            description = data["description"];
+            name = data["id"];
+            date = data["effective"];
+            $("#sci-hi-name").text(name);
+            $("#sci-hi-desc").text(description);
+            $("#sci-hi-img").attr("src",image);
+            $("#sci-hi-anchor").attr("href",image);
+            $("#sci-hi-caption").text(image_caption);
+            $("#sci-hi-contents").text(content );
+            $("#sci-hi-date").text("Effective: " + date);
+            $("#sci-hi-link").attr("href", ALMA_BASE_URL + 'science-highlights/highlights');
+        }
+    });
+}
+    
+    
+function getScienceHighlightURL(){
+    console.log( "getScienceHighlightURL()");
+    var scihiURL = ALMA_BASE_URL + 'science-highlights/highlights';
+    $.ajax({
+        url: scihiURL,
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('Accept', 'application/json');
+        },
+        success: function(data) {
+            shURL = data["items"][0]["@id"];
+            getScienceHighlight(shURL);
+        }
+    });
+}
